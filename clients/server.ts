9df7e17695472663f7ae84c358ce284b5ae9ed26
Scott Jones <scott.jones@EF.com>
2018-02-26 12:53:15 -0500

    git some mfkin nextjs up in here

---
+++ clients/server.ts
@@ -0,0 +1,11 @@
+import appRoutes from "./routes";
+import * as next from "next";
+import * as Express from "express";
+
+const app = next({dev: process.env.NODE_ENV !== "production"});
+const handler = appRoutes.getRequestHandler(app);
+
+const express = Express();
+app.prepare().then( () => {
+    express.use(handler).listen(8080);
+});
